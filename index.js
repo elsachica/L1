cd5e8e88e95c267af09cb2b0507ceca30095132f
Elsa Gas Wikström <eg223ps@student.lnu.se>
2024-09-03 16:15:09 +0200

    Name not showing in terminal

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+import readline from 'readline'
+import { Welcome } from './src/welcome.js'
 
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
+
+const welcome = new Welcome()
+
+rl.question('Vad heter du? ', (name) => {
+  welcome.greet(name)
+
+  rl.close()
+})
